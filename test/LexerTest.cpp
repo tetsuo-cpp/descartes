d7c237f6def1b593013801233abba7f2e3a768f5
Alex Cameron <ascottcameron@gmail.com>
2021-03-07 23:05:23 +1100

    Setup unit testing with Catch2

---
+++ test/LexerTest.cpp
@@ -0,0 +1,13 @@
+#include <Lexer.h>
+
+#include <catch2/catch.hpp>
+
+namespace descartes::test {
+
+TEST_CASE("lexer test", "[lexer]") {
+  int i = 0;
+  CHECK(i == 0);
+  CHECK(true);
+}
+
+} // namespace descartes::test
