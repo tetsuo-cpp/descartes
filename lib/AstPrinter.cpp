7d61e060800312f8c7727d8e35b34f7623f389e0
Alex Cameron <ascottcameron@gmail.com>
2021-04-18 21:11:35 +1000

    Update AST printer to support new node types

--- lib/AstPrinter.cpp
+++ lib/AstPrinter.cpp
@@ -14,17 +14,6 @@ void AstPrinter::printBlock(Block &block) {
   std::cout << obj.dump(indentWidth) << "\n";
 }
 
-json AstPrinter::convertExpr(Expr &expr) {
-  if (auto *stringLiteral = exprCast<StringLiteral *>(expr))
-    return convertStringLiteral(*stringLiteral);
-  else if (auto *numberLiteral = exprCast<NumberLiteral *>(expr))
-    return convertNumberLiteral(*numberLiteral);
-  else if (auto *varRef = exprCast<VarRef *>(expr))
-    return convertVarRef(*varRef);
-  else
-    assert(!"Unknown node in AstPrinter.");
-}
-
 json AstPrinter::convertBlock(Block &block) {
   json blockObj;
   blockObj["Type"] = "Block";
@@ -69,32 +58,11 @@ json AstPrinter::convertTypeDefs(TypeDefs &typeDefs) {
 }
 
 json AstPrinter::convertVarDecl(VarDecl &varDecl) {
-  // TODO: Print type specifiers.
   json varDeclObj;
   varDeclObj["Type"] = "VarDecl";
   varDeclObj["Identifier"] = varDecl.identifier.getName();
+  varDeclObj["Type"] = varDecl.type.getName();
   return varDeclObj;
-}
-
-json AstPrinter::convertStringLiteral(StringLiteral &stringLiteral) {
-  json stringLiteralObj;
-  stringLiteralObj["Type"] = "StringLiteral";
-  stringLiteralObj["Val"] = stringLiteral.val;
-  return stringLiteralObj;
-}
-
-json AstPrinter::convertNumberLiteral(NumberLiteral &numberLiteral) {
-  json numberLiteralObj;
-  numberLiteralObj["Type"] = "NumberLiteral";
-  numberLiteralObj["Val"] = numberLiteral.val;
-  return numberLiteralObj;
-}
-
-json AstPrinter::convertVarRef(VarRef &varRef) {
-  json varRefObj;
-  varRefObj["Type"] = "VarRef";
-  varRefObj["Identifier"] = varRef.identifier.getName();
-  return varRefObj;
 }
 
 json AstPrinter::convertFunction(Function &function) {
@@ -111,6 +79,162 @@ json AstPrinter::convertFunction(Function &function) {
   functionObj["Args"] = args;
   functionObj["Block"] = convertBlock(function.block);
   return functionObj;
+}
+
+json AstPrinter::convertStatement(Statement &statement) {
+  switch (statement.getKind()) {
+  case StatementKind::Assignment:
+    return convertAssignment(statement);
+  case StatementKind::Compound:
+    return convertCompound(statement);
+  case StatementKind::If:
+    return convertIf(statement);
+  case StatementKind::While:
+    return convertWhile(statement);
+  case StatementKind::Call:
+    return convertCallStatement(statement);
+  default:
+    assert(!"Unsupported statement type");
+  }
+}
+
+json AstPrinter::convertAssignment(Statement &statement) {
+  auto *assignment = statementCast<Assignment *>(statement);
+  assert(assignment);
+  json assignmentObj;
+  assignmentObj["Left"] = convertExpr(*assignment->lhs);
+  assignmentObj["Right"] = convertExpr(*assignment->rhs);
+  return assignmentObj;
+}
+
+json AstPrinter::convertCompound(Statement &statement) {
+  auto *compound = statementCast<Compound *>(statement);
+  assert(compound);
+  json compoundObj = json::array();
+  for (const auto &s : compound->body)
+    compoundObj.emplace_back(convertStatement(*s));
+  return compoundObj;
+}
+
+json AstPrinter::convertIf(Statement &statement) {
+  auto *ifStatement = statementCast<If *>(statement);
+  assert(ifStatement);
+  json ifObj;
+  ifObj["Type"] = "If";
+  ifObj["Cond"] = convertExpr(*ifStatement->cond);
+  ifObj["Then"] = convertStatement(*ifStatement->thenStatement);
+  if (ifStatement->elseStatement)
+    ifObj["Else"] = convertStatement(*ifStatement->elseStatement);
+  return ifObj;
+}
+
+json AstPrinter::convertWhile(Statement &statement) {
+  auto *whileStatement = statementCast<While *>(statement);
+  assert(whileStatement);
+  json whileObj;
+  whileObj["Type"] = "While";
+  whileObj["Cond"] = convertExpr(*whileStatement->cond);
+  whileObj["Body"] = convertStatement(*whileStatement->body);
+  return whileObj;
+}
+
+json AstPrinter::convertFor(Statement &statement) {
+  auto *forStatement = statementCast<For *>(statement);
+  assert(forStatement);
+  json forObj;
+  forObj["Type"] = "For";
+  forObj["Begin"] = convertExpr(*forStatement->begin);
+  forObj["End"] = convertExpr(*forStatement->end);
+  forObj["To"] = forStatement->to;
+  forObj["Body"] = convertStatement(*forStatement->body);
+  return forObj;
+}
+
+json AstPrinter::convertCallStatement(Statement &statement) {
+  auto *call = statementCast<CallStatement *>(statement);
+  assert(call);
+  json callObj;
+  callObj["Type"] = "CallStatement";
+  callObj["Call"] = convertExpr(*call->call);
+  return callObj;
+}
+
+json AstPrinter::convertExpr(Expr &expr) {
+  switch (expr.getKind()) {
+  case ExprKind::StringLiteral:
+    return convertStringLiteral(expr);
+  case ExprKind::NumberLiteral:
+    return convertNumberLiteral(expr);
+  case ExprKind::VarRef:
+    return convertVarRef(expr);
+  case ExprKind::BinaryOp:
+    return convertBinaryOp(expr);
+  case ExprKind::Call:
+    return convertCall(expr);
+  case ExprKind::MemberRef:
+    return convertMemberRef(expr);
+  }
+}
+
+json AstPrinter::convertStringLiteral(Expr &expr) {
+  auto *stringLiteral = exprCast<StringLiteral *>(expr);
+  assert(stringLiteral);
+  json stringLiteralObj;
+  stringLiteralObj["Type"] = "StringLiteral";
+  stringLiteralObj["Val"] = stringLiteral->val;
+  return stringLiteralObj;
+}
+
+json AstPrinter::convertNumberLiteral(Expr &expr) {
+  auto *numberLiteral = exprCast<NumberLiteral *>(expr);
+  assert(numberLiteral);
+  json numberLiteralObj;
+  numberLiteralObj["Type"] = "NumberLiteral";
+  numberLiteralObj["Val"] = numberLiteral->val;
+  return numberLiteralObj;
+}
+
+json AstPrinter::convertVarRef(Expr &expr) {
+  auto *varRef = exprCast<VarRef *>(expr);
+  assert(varRef);
+  json varRefObj;
+  varRefObj["Type"] = "VarRef";
+  varRefObj["Identifier"] = varRef->identifier.getName();
+  return varRefObj;
+}
+
+json AstPrinter::convertBinaryOp(Expr &expr) {
+  auto *binaryOp = exprCast<BinaryOp *>(expr);
+  assert(binaryOp);
+  json binaryOpObj;
+  binaryOpObj["Type"] = "BinaryOp";
+  binaryOpObj["Left"] = convertExpr(*binaryOp->lhs);
+  binaryOpObj["Right"] = convertExpr(*binaryOp->rhs);
+  binaryOpObj["Operator"] = binaryOpKindToString(binaryOp->kind);
+  return binaryOpObj;
+}
+
+json AstPrinter::convertCall(Expr &expr) {
+  auto *call = exprCast<Call *>(expr);
+  assert(call);
+  json callObj;
+  callObj["Type"] = "Call";
+  callObj["Name"] = call->functionName.getName();
+  json argObjs;
+  for (const auto &arg : call->args)
+    argObjs.emplace_back(convertExpr(*arg));
+  callObj["Args"] = argObjs;
+  return callObj;
+}
+
+json AstPrinter::convertMemberRef(Expr &expr) {
+  auto *memberRef = exprCast<MemberRef *>(expr);
+  assert(memberRef);
+  json memberRefObj;
+  memberRefObj["Type"] = "MemberRef";
+  memberRefObj["Expr"] = convertExpr(*memberRef->expr);
+  memberRefObj["Identifier"] = memberRef->identifier.getName();
+  return memberRefObj;
 }
 
 } // namespace descartes
--- lib/AstPrinter.h
+++ lib/AstPrinter.h
@@ -13,15 +13,25 @@ public:
 
 private:
   using json = nlohmann::json;
-  json convertExpr(Expr &expr);
   json convertBlock(Block &block);
   json convertConstDef(ConstDef &constDef);
   json convertTypeDefs(TypeDefs &typeDefs);
   json convertVarDecl(VarDecl &varDecl);
-  json convertStringLiteral(StringLiteral &stringLiteral);
-  json convertNumberLiteral(NumberLiteral &numberLiteral);
-  json convertVarRef(VarRef &varRef);
   json convertFunction(Function &function);
+  json convertStatement(Statement &statement);
+  json convertAssignment(Statement &statement);
+  json convertCompound(Statement &statement);
+  json convertIf(Statement &statement);
+  json convertWhile(Statement &statement);
+  json convertFor(Statement &statement);
+  json convertCallStatement(Statement &statement);
+  json convertExpr(Expr &expr);
+  json convertStringLiteral(Expr &expr);
+  json convertNumberLiteral(Expr &expr);
+  json convertVarRef(Expr &expr);
+  json convertBinaryOp(Expr &expr);
+  json convertCall(Expr &expr);
+  json convertMemberRef(Expr &expr);
 };
 
 } // namespace descartes
